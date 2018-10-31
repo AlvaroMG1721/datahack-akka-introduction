adff80d23850803028a4fbf2bad6a67049db8732
Álvaro Márquez <alvaro.marquez.gomez@everis.com>
2018-10-31 21:31:01 +0100

    Inicio ejemplo práctica actores.

--- src/main/scala/com/datahack/akka/introduction/Boot.scala
+++ src/main/scala/com/datahack/akka/introduction/Boot.scala
@@ -1,5 +1,31 @@
 package com.datahack.akka.introduction
 
+import akka.actor.{ActorRef, ActorSystem, Props}
+import com.datahack.akka.introduction.actors.Student.PerformAnAdviceRequest
+import com.datahack.akka.introduction.actors.{Student, Teacher}
+import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.duration._
+
 object Boot extends App {
+
+  // Initialize the ActorSystem
+  val actorSystem = ActorSystem("UniversityMessageSystem")
+
+  // Whe need an execution context to perform the concurrent code
+  implicit val executionContext: ExecutionContextExecutor = actorSystem.dispatcher
+
+  // Construct the Actor Refs
+  val teacherActorRef: ActorRef = actorSystem.actorOf(Props[Teacher], "teacher")
+  val studentActorRef: ActorRef = actorSystem.actorOf(Props(classOf[Student], teacherActorRef), "student")
+
+  actorSystem.scheduler.schedule(
+    5 seconds,
+    15 seconds,
+    studentActorRef,
+    PerformAnAdviceRequest
+  )
+
+  // Ensure that the constructed ActorSystem is shut down when the JVM shuts down
+  sys.addShutdownHook(actorSystem.terminate())
 
 }
---
+++ src/main/scala/com/datahack/akka/introduction/actors/Student.scala
@@ -0,0 +1,31 @@
+package com.datahack.akka.introduction.actors
+
+import akka.actor.{Actor, ActorLogging, ActorRef}
+import com.datahack.akka.introduction.actors.Student.PerformAnAdviceRequest
+import com.datahack.akka.introduction.actors.Teacher.{Advice, AskAdvice, IDoNotUnderstand}
+import org.scalacheck.Gen
+
+object Student {
+  case object PerformAnAdviceRequest
+}
+
+class Student(teacher: ActorRef) extends Actor with ActorLogging {
+
+  log.debug(s"${self.path} actor created") // self es referencia al propio actor, propio de AKKA
+
+  // Generate random topics
+  val genTopics: Gen[String] = Gen.oneOf("History", "Maths", "Geography", "Physics", "Literature", "Love")
+
+  override def receive: Receive = {
+    case PerformAnAdviceRequest => {
+      val topic = genTopics.sample.get
+      teacher ! AskAdvice(topic)
+    }
+    case Advice(text) => {
+      log.info(s"The requested advice: $text")
+    }
+    case IDoNotUnderstand => {
+      log.error("I do not understand.")
+    }
+  }
+}
